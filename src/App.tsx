783b0c45e625776ddf51fff2cd9b88f5d487a274
Darek Mazur <dev@darekmazur.it>
2024-03-27 04:09:59 +0100

    Add basic styles using MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,16 @@
-import { ThemeProvider, Typography } from '@mui/material';
+import {
+  Container,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TablePagination,
+  TableRow,
+  ThemeProvider,
+  Typography,
+} from '@mui/material';
 import { mockTags } from './mocks/mocks.ts';
 import theme from './providers/ThemeProvider.ts';
 
@@ -8,45 +20,66 @@ const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <Typography>
-        <header>
-          <h1>Lorem ipsum</h1>
-        </header>
-        <main>
-          <span>Tags per page</span>
-          <ul>
-            {perPageCount.map((count) => (
-              <li key={count}>{count}</li>
-            ))}
-          </ul>
-          <table>
-            <thead>
-              <tr>
-                <th>Tag</th>
-                <th>Posts count</th>
-                <th>LINK</th>
-              </tr>
-            </thead>
-            <tbody>
-              {mockTags.items.map((tag) => (
-                <tr key={tag.name}>
-                  <th>{tag.name}</th>
-                  <td>{new Intl.NumberFormat().format(tag.count)}</td>
-                  <td>
-                    <a
-                      href={`https://stackoverflow.com/questions/tagged/${tag.name}`}
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      icon
-                    </a>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          <div>pagination</div>
-        </main>
-        <footer>&copy; 2024 Darek Mazur</footer>
+        <Container component="header">
+          <Typography variant="h2" component="h1">
+            Lorem ipsum
+          </Typography>
+        </Container>
+        <Container component="main">
+          <TablePagination
+            rowsPerPageOptions={perPageCount}
+            component="div"
+            count={mockTags.items.length}
+            rowsPerPage={5}
+            page={0}
+            onPageChange={() => {}}
+            onRowsPerPageChange={() => {}}
+            labelRowsPerPage="Tags per page"
+          />
+          <Paper elevation={2}>
+            <TableContainer>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Tag</TableCell>
+                    <TableCell>Posts count</TableCell>
+                    <TableCell>LINK</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {mockTags.items.map((tag) => (
+                    <TableRow key={tag.name}>
+                      <TableCell>{tag.name}</TableCell>
+                      <TableCell>
+                        {new Intl.NumberFormat().format(tag.count)}
+                      </TableCell>
+                      <TableCell>
+                        <a
+                          href={`https://stackoverflow.com/questions/tagged/${tag.name}`}
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          icon
+                        </a>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Paper>
+        </Container>
+        <Container
+          component="footer"
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '5rem',
+          }}
+        >
+          &copy; 2024 Darek Mazur
+        </Container>
       </Typography>
     </ThemeProvider>
   );
