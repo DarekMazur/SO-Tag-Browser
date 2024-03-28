9cb58ff02fe7d86d13820d6f674113492dc96ec6
Darek Mazur <dev@darekmazur.it>
2024-03-28 18:53:56 +0100

    Avoid a layout jump when reaching the last page with empty rows

--- src/components/Organisms/TableContent/TableContent.tsx
+++ src/components/Organisms/TableContent/TableContent.tsx
@@ -1,4 +1,4 @@
-import { TableBody, TableRow } from '@mui/material';
+import { TableBody, TableCell, TableRow } from '@mui/material';
 import { useSelector } from 'react-redux';
 import { mockTags } from '../../../mocks/mocks.ts';
 import TagLink from '../../Atoms/TagLink/TagLink.tsx';
@@ -8,6 +8,11 @@ import { RootState } from '../../../store';
 const TableContent = () => {
   const pagination = useSelector((state: RootState) => state.pagination);
   const tagsPerPage = useSelector((state: RootState) => state.tagsPerPage);
+
+  const emptyRows =
+    pagination > 0
+      ? Math.max(0, (1 + pagination) * tagsPerPage - mockTags.items.length)
+      : 0;
 
   return (
     <TableBody>
@@ -25,6 +30,11 @@ const TableContent = () => {
           <TableItem label={<TagLink to={tag.name} />} />
         </TableRow>
       ))}
+      {emptyRows > 0 && (
+        <TableRow style={{ height: 53 * emptyRows }}>
+          <TableCell colSpan={6} />
+        </TableRow>
+      )}
     </TableBody>
   );
 };
