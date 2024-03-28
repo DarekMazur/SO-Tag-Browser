83266607f9a09a217a26129489e595ddd5b8c9f1
Darek Mazur <dev@darekmazur.it>
2024-03-28 19:48:57 +0100

    Add sorting label

--- src/components/Molecules/TableHeader/TableHeader.tsx
+++ src/components/Molecules/TableHeader/TableHeader.tsx
@@ -1,4 +1,7 @@
-import { TableCell, TableHead, TableRow } from '@mui/material';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import TableSortLabel from '@mui/material/TableSortLabel';
 import { FC } from 'react';
 import { ITableHeaders } from '../../../types/types.ts';
 
@@ -7,7 +10,16 @@ const TableHeader: FC<ITableHeaders> = ({ headers }) => {
     <TableHead>
       <TableRow>
         {headers.map((header) => (
-          <TableCell key={header || 'link'}>{header}</TableCell>
+          <TableCell key={header || 'link'}>
+            <TableSortLabel
+              active={false}
+              hideSortIcon={!header}
+              direction="asc"
+              onClick={() => {}}
+            >
+              {header}
+            </TableSortLabel>
+          </TableCell>
         ))}
       </TableRow>
     </TableHead>
