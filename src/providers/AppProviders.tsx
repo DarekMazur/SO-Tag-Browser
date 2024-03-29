defe5b5077409700925222ebed1391523c90845a
Darek Mazur <dev@darekmazur.it>
2024-03-29 07:55:18 +0100

    Prepare app translations

--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -1,8 +1,11 @@
 import { FC, ReactNode } from 'react';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { Provider } from 'react-redux';
+import { I18nextProvider } from 'react-i18next';
+import i18next from 'i18next';
 import theme from './ThemeProvider.ts';
 import { store } from '../store';
+import '../i18n/config.ts';
 
 interface IAppProviders {
   children: ReactNode;
@@ -10,12 +13,14 @@ interface IAppProviders {
 
 const AppProviders: FC<IAppProviders> = ({ children }) => {
   return (
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        {children}
-      </ThemeProvider>
-    </Provider>
+    <I18nextProvider i18n={i18next}>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          {children}
+        </ThemeProvider>
+      </Provider>
+    </I18nextProvider>
   );
 };
 
