d340d38037874242a31577e26a6e5033dd194b0a
Darek Mazur <dev@darekmazur.it>
2024-03-29 05:27:20 +0100

    Fix footer styles

--- src/components/Organisms/Footer/Footer.tsx
+++ src/components/Organisms/Footer/Footer.tsx
@@ -2,16 +2,15 @@ import { Container } from '@mui/material';
 import Copyrights from '../../Atoms/Copyrights/Copyrights.tsx';
 
 const Footer = () => {
+  const styledFooterWrapper = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '5rem',
+  };
+
   return (
-    <Container
-      component="footer"
-      sx={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '5rem',
-      }}
-    >
+    <Container component="footer" sx={styledFooterWrapper}>
       <Copyrights />
     </Container>
   );
