270be533153c9ad5e5a6d9c088d4e2ad7cf5ca8b
Darek Mazur <dev@darekmazur.it>
2024-03-29 03:17:44 +0100

    Remove comments

--- src/components/Molecules/TableHeader/TableHeader.tsx
+++ src/components/Molecules/TableHeader/TableHeader.tsx
@@ -12,33 +12,6 @@ const TableHeader: FC<ITableHeeadersProps> = ({ headers }) => {
   const orderBy = useSelector((state: RootState) => state.orderBy);
   const dispatch = useDispatch();
 
-  // const descendingComparator = <T,>(a: T, b: T, orderKey: keyof T) => {
-  //   if (b[orderKey] < a[orderKey]) {
-  //     return -1;
-  //   }
-  //   if (b[orderKey] > a[orderKey]) {
-  //     return 1;
-  //   }
-  //   return 0;
-  // };
-
-  // const getComparator = <Key extends keyof never>(
-  //   orderKey: Key,
-  // ): ((
-  //   a: { [key in Key]: number | string },
-  //   b: { [key in Key]: number | string },
-  // ) => number) => {
-  //   return order === 'desc'
-  //     ? (a, b) => descendingComparator(a, b, orderKey)
-  //     : (a, b) => -descendingComparator(a, b, orderKey);
-  // };
-
-  // const handleRequestSort = (_event: MouseEvent<unknown>, property: string) => {
-  //   const isAsc = orderBy === property && order === 'asc';
-  //   dispatch(setOrder(isAsc ? 'desc' : 'asc'));
-  //   dispatch(setOrderBy(property));
-  // };
-
   const createSortHandler = (property: string) => () => {
     const isAsc = orderBy === property && order === 'asc';
     dispatch(setOrder(isAsc ? 'desc' : 'asc'));
@@ -47,7 +20,6 @@ const TableHeader: FC<ITableHeeadersProps> = ({ headers }) => {
 
   return (
     <TableHead>
-      {/* {console.log(keyof mockTags.items)} */}
       <TableRow>
         {headers.map((header) => (
           <TableCell key={header.name}>
