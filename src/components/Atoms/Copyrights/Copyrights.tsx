f15786cdeb45ea60ae50465537231fb1d7215d97
Darek Mazur <dev@darekmazur.it>
2024-03-29 02:24:52 +0100

    Get copyright year based on current date

--- src/components/Atoms/Copyrights/Copyrights.tsx
+++ src/components/Atoms/Copyrights/Copyrights.tsx
@@ -1,5 +1,9 @@
 const Copyrights = () => {
-  return <>&copy; 2024 Darek Mazur</>;
+  const getFullYear = () => {
+    const today = new Date();
+    return today.getFullYear();
+  };
+  return <>&copy; {getFullYear()} Darek Mazur</>;
 };
 
 export default Copyrights;
