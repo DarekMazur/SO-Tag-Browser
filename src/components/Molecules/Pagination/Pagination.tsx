085009a64cd7aa6b55df9631fd649f46a0eb4c0a
Darek Mazur <dev@darekmazur.it>
2024-03-30 04:41:29 +0100

    Change tags per page dropdown to input

--- src/components/Molecules/Pagination/Pagination.tsx
+++ src/components/Molecules/Pagination/Pagination.tsx
@@ -2,6 +2,8 @@ import { ChangeEvent, MouseEvent } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import TablePagination from '@mui/material/TablePagination';
 import { useTranslation } from 'react-i18next';
+import { Container } from '@mui/material';
+import Box from '@mui/material/Box';
 import {
   RootState,
   setPage,
@@ -16,7 +18,6 @@ const Pagination = () => {
   const tagsPerPage = useSelector((state: RootState) => state.tagsPerPage);
   const dispatch = useDispatch();
   const { data: tags } = useGetTagsQuery();
-  const perPageCount: number[] = [5, 15, 25, 50, 100];
 
   const handleChangePage = (
     _event: MouseEvent<HTMLButtonElement> | null,
@@ -25,25 +26,39 @@ const Pagination = () => {
     dispatch(setPage(newPage));
   };
 
-  const handleChangeRowsPerPage = (
-    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-  ) => {
+  const handleChangeRowsPerPage = (event: ChangeEvent<HTMLInputElement>) => {
     dispatch(setTagsPerPage(Number(event.target.value)));
     dispatch(setPage(0));
   };
 
   return (
-    <TablePagination
-      rowsPerPageOptions={perPageCount}
-      component="div"
-      count={tags ? tags.items.length : 0}
-      rowsPerPage={tagsPerPage}
-      page={page}
-      onPageChange={handleChangePage}
-      onRowsPerPageChange={handleChangeRowsPerPage}
-      labelRowsPerPage={t('table.pagination.label')}
-      ActionsComponent={PaginationActions}
-    />
+    <Container
+      sx={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}
+    >
+      <Box
+        sx={{
+          fontSize: '1rem',
+          '& input': { width: '60px', marginLeft: '10px' },
+        }}
+      >
+        {t('table.pagination.label')}
+        <input
+          type="number"
+          min={0}
+          onChange={handleChangeRowsPerPage}
+          value={tagsPerPage}
+        />
+      </Box>
+      <TablePagination
+        rowsPerPageOptions={[]}
+        component="div"
+        count={tags ? tags.items.length : 0}
+        rowsPerPage={tagsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        ActionsComponent={PaginationActions}
+      />
+    </Container>
   );
 };
 
