4cf1a90d60d7a0456c52f3148d62efce800d9b49
Darek Mazur <dev@darekmazur.it>
2024-03-30 02:19:26 +0100

    Test TableContent component

--- src/components/Organisms/TableWrapper/TableWrapper.test.tsx
+++ src/components/Organisms/TableWrapper/TableWrapper.test.tsx
@@ -6,25 +6,20 @@ describe('Search Bar', () => {
     const { container } = render(<TableWrapper />);
     expect(container).toMatchSnapshot();
   });
-
-  // eslint-disable-next-line vitest/expect-expect
   it('Displays default items range in pagination', async () => {
     render(<TableWrapper />);
     await screen.findByText(/1–5/);
   });
-  // eslint-disable-next-line vitest/expect-expect
   it('Displays total items in pagination', async () => {
     render(<TableWrapper />);
     await screen.findByText(/20/);
   });
-  // eslint-disable-next-line vitest/expect-expect
   it('Displays next page after click in pagination', async () => {
     render(<TableWrapper />);
     const nextButton = screen.getByLabelText('next page');
     fireEvent.click(nextButton);
     await screen.findByText(/6–10/);
   });
-  // eslint-disable-next-line vitest/expect-expect
   it('Displays table headers', async () => {
     render(<TableWrapper />);
     await screen.findByText(/Posts count/);
